435343cf21f05eabc1ba50ff8792a13c34cb1bb7
jamppajoo <mummo94@hotmail.com>
2017-03-15 09:21:21 +0200

    Small changes

--- app/src/main/java/com/jumuvirma/javaexercise/MainActivity.java
+++ app/src/main/java/com/jumuvirma/javaexercise/MainActivity.java
@@ -4,29 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,9 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> messages = new ArrayList<>();
     ArrayList<String> dates = new ArrayList<>();
-
-    ArrayAdapter<String> adapter;
-    ArrayAdapter<String> subAdapter;
     ListView listView;
 
 
